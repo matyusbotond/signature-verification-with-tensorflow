aec461f47763d928c36a853d712e98874e34460c
Mátyus Botond <matyusbotond@outlook.hu>
2016-04-08 06:23:07 -0700

    SignatureSample class

--- SVC_convert.py
+++ SVC_convert.py
@@ -1,9 +1,32 @@
+class SignatureSample:
+    def __init__(self, isOriginal, image):
+        self.isOriginal = isOriginal
+        self.image = image
+
 def read_image(path):
     import cv2
     import numpy as np
-    image = cv2.imread(path)
+    
+    image = cv2.imread(path)[0:180,0:600]
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return gray_image.flatten(), [0,1];
+    
+    #cv2.imshow("cropped",gray_image)
+    #cv2.waitKey(0)
+    
+    #print path + ", " + str(gray_image.flatten().shape)
+    
+    return SignatureSample("e" in path, gray_image.flatten());
+    
+def get_random_batch(datas, count):
+    from random import randint
+    
+    signatures = []
+        
+    for c in range(0,count):
+        i = randint(0,len(datas)-1)
+        signatures.append(datas[i])
+        
+    return signatures
     
 def createSignatureDataSet(signature_path):
     import numpy as np
@@ -11,14 +34,24 @@ def createSignatureDataSet(signature_path):
     
     image_files = glob.glob(signature_path + "*.png")
     
-    raw_data = []
-    label = []
+    signatures = []
     
     for image_path in image_files:
         signature = read_image(image_path)
-        raw_data.append(signature[0])
-        label.append(signature[1])
+        signatures.append(signature)
+
+    return signatures
     
-    return raw_data, label
-    
-#print createSignatureDataSet("SVC/002/")
+def getSignatureImagesAndLabels(signatures):
+    import numpy as np
+
+    images = []
+    labels = []
+    for signature in signatures:
+        images.append(signature.image)
+        if(signature.isOriginal):
+            labels.append([1,0])
+        else:
+            labels.append([0,1])
+        
+    return np.array(images), np.array(labels)
